3d1fd75902d6cd0a9b168bfb7f3d5d83f1232770
anderson_paschoalon <anderson_paschoalon@hst.com.br>
2021-01-31 15:24:34 -0300

    Alura

--- Alura/ASP.Net Core pt1/web/Startup.cs
+++ Alura/ASP.Net Core pt1/web/Startup.cs
@@ -12,29 +12,81 @@ namespace web
 {
     public class Startup
     {
+        enum runMethod
+        { 
+            RUN,
+            ENDPOINTS
+        };
+
+        enum serviceType
+        {
+            TRANSIENT,
+            SCOPE,
+            SINGLETON
+        };
+
+        const runMethod RUN = runMethod.RUN;
+        const serviceType SERVICE = serviceType.TRANSIENT;
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            if (SERVICE == serviceType.TRANSIENT)
+            {
+                services.AddTransient<ICatalogo, Catalogo>();
+                services.AddTransient<IRelatorio, Relatorio>();
+            }
+            else if (SERVICE == serviceType.SCOPE)
+            {
+                services.AddScoped<ICatalogo, Catalogo>();
+                services.AddScoped<IRelatorio, Relatorio>();
+            }
+            else if (SERVICE == serviceType.SINGLETON)
+            {
+
+                var cat = new Catalogo();
+                services.AddSingleton<ICatalogo>(cat);
+                services.AddSingleton<IRelatorio>(new Relatorio(cat));
+
+            }
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProfiver)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            bool test01 = false;
+            bool test02 = true;
+            ICatalogo catalogo = serviceProfiver.GetService<ICatalogo>();
+            IRelatorio relatorio = serviceProfiver.GetService<IRelatorio>();
 
-            app.UseEndpoints(endpoints =>
+
+            if (RUN == runMethod.ENDPOINTS)
             {
-                endpoints.MapGet("/", async context =>
+                app.UseRouting();
+                app.UseEndpoints(endpoints =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    endpoints.MapGet("/", async context =>
+                    {
+                        await relatorio.renderHtml(context);
+                    });
                 });
-            });
+            }
+            if (RUN == runMethod.RUN)
+            {
+                app.Run(async (context) =>
+                {
+                    await relatorio.renderHtml(context);
+                });
+            }
+
         }
     }
 }
