4b866601edb67176f5d2bd634f91eacb1077334c
anderson_paschoalon <anderson_paschoalon@hst.com.br>
2021-02-09 09:02:47 -0300

    pt3 completa

---
+++ Alura/ASP.Net Core pt1/CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+        // /pedido/cadastro
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        // /pedido/carrinho
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+
+        // /pedido/carrossel
+        public IActionResult Carrossel()
+        {
+            return View();
+        }
+
+        // /pedido/resumo
+        public IActionResult Resumo()
+        {
+            return View();
+        }
+    }
+}
