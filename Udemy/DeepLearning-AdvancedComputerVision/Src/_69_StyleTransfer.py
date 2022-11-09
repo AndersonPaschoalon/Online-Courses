ee99d39ad164a21ed5906d8b1556693b76cba7e1
anderson_paschoalon <anderson_paschoalon@hst.com.br>
2022-11-09 18:17:13 -0300

    ajustes para rodar o codigo em diversas configurações

--- Udemy/DeepLearning-AdvancedComputerVision/Src/_69_StyleTransfer.py
+++ Udemy/DeepLearning-AdvancedComputerVision/Src/_69_StyleTransfer.py
@@ -2,6 +2,8 @@
 # https://www.udemy.com/advanced-computer-vision
 
 from __future__ import print_function, division
+
+import sys
 from builtins import range, input
 # Note: you may need to update your version of future
 # sudo pip install -U future
@@ -23,13 +25,13 @@ import matplotlib.pyplot as plt
 
 from scipy.optimize import fmin_l_bfgs_b
 import datetime
-
+import os
 
 import tensorflow as tf
 if tf.__version__.startswith('2'):
     tf.compat.v1.disable_eager_execution()
 
-
+GPU_REQUIRED_TF_VERSION = "2.6"
 
 def VGG16_AvgPool(shape):
     """
@@ -124,7 +126,85 @@ def get_loss_and_grads_wrapper(x_vec):
     l, g = get_loss_and_grads([x_vec.reshape(*batch_shape)])
     return l.astype(np.float64), g.flatten().astype(np.float64)
 
+def run_style_transfer_pt1(out_dir, image_file, nn_cutoff):
+    # print run information
+    print("Running style transfer -> out_dir:", out_dir, ", image_file:", image_file, ", nn_cutoff:", nn_cutoff)
 
+    # parse vars
+    out_dir = out_dir + "\\"
+    path = os.path.join(out_dir, image_file)
+    image_name = os.path.basename(os.path.splitext()[0])
+
+    # load image
+    img = None
+    tf_version = str(tf.__version__)
+    if tf_version.startswith(GPU_REQUIRED_TF_VERSION):
+        print("Tensorflow version for GPU usage matches... running on GPU mode...")
+        img = image.load_img(path)
+    else:
+        print("Running on CPU mode")
+        img = tf.keras.utils.load_img(path)
+
+    # convert the image to array and process for vgg
+    # x = image.img_to_array(img)
+    x = tf.keras.utils.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+
+    # we'll use this through the rest of the script
+    batch_shape = x.shape
+    shape = x.shape[1:]
+
+    # make a content model
+    # try different cutoffs to see the images that result
+    # nn_cutoff = 11
+    content_model = VGG16_AvgPool_CutOff(shape, nn_cutoff)
+
+    # make the target
+    target = K.variable(content_model.predict(x))
+
+    # try to match the image
+
+    # define our loss in keras
+    #loss = K.mean(K.square(target=content_model.output))
+    loss = K.mean(K.square(target - content_model.output))
+
+    # gradients which are needed by the optimizer
+    grads = K.gradients(loss, content_model.input)
+    # grads = K.gradients(loss, content_model.trainable_variables)
+
+    # just like theano funcion
+    get_loss_and_grads = K.function(inputs=[content_model.input],
+                                    outputs=[loss] + grads)
+
+    # generate the images
+    t0 = datetime.now()
+    losses = []
+    x = np.random.randn(np.prod(batch_shape))
+    for i in range(10):
+        x, l, _ = fmin_l_bfgs_b(func=get_loss_and_grads_wrapper,
+                                x0=x,
+                                # bounds = [[-127, 127]]*len(x.flatten()),
+                                maxfun=20)
+        x = np.clip(x, -127, 127)
+        # print("min:", x.min(), "max:", x.max())
+        print("iter=%s, loss=%s" % (i, l))
+        losses.append(l)
+
+    print("duration:", datetime.now() - t0)
+
+    plt.clf()
+    plt.plot(losses)
+    plt.savefig(out_dir + "losses.jpg")
+
+    plt.clf()
+    newimg = x.reshape(*batch_shape)
+    final_img = unpreprocess(newimg)
+    plt.imshow(scale_img(final_img[0]))
+    plt.savefig(out_dir + "final_img" + "_" + img_name + "_" + str(nn_cutoff) + ".jpg")
+
+
+"""
 if __name__ == '__main__':
     print("TensorFlow version:", tf.__version__)
     # open an image
@@ -172,7 +252,7 @@ if __name__ == '__main__':
                                     outputs=[loss] + grads)
 
     # generate the images
-    #t0 = datetime.now()
+    t0 = datetime.now()
     losses = []
     x = np.random.randn(np.prod(batch_shape))
     for i in range(10):
@@ -185,7 +265,7 @@ if __name__ == '__main__':
         print("iter=%s, loss=%s" % (i, l))
         losses.append(l)
 
-    #print("duration:", datetime.now() - t0)
+    print("duration:", datetime.now() - t0)
 
     plt.clf()
     plt.plot(losses)
@@ -197,10 +277,38 @@ if __name__ == '__main__':
     plt.imshow(scale_img(final_img[0]))
     plt.savefig(out_dir + "final_img" + "_" + img_name + "_" + str(nn_cutoff) + ".jpg")
 
+"""
 
+if __name__ == '__main__':
+    print("* For running on CPU, it is required Python 3.9 and TensorFlow 2.10.0")
+    print("* For running on GPU, it is required Python 3.6 and TensorFlow 2.6")
+    print("")
+    print("TensorFlow version:", tf.__version__)
+    print("Python Version: ", sys.version)
+    print("Python Version Info: ", sys.version_info)
+    # open an image
 
+    print("*ELEFANTE*")
+    run_style_transfer_pt1(out_dir="69",
+                           image_file="elefante.jpg",
+                           nn_cutoff=11)
+    run_style_transfer_pt1(out_dir="69",
+                           image_file="elefante.jpg",
+                           nn_cutoff=13)
+    run_style_transfer_pt1(out_dir="69",
+                           image_file="elefante.jpg",
+                           nn_cutoff=5)
 
-
+    print("*GATO*")
+    run_style_transfer_pt1(out_dir="69",
+                           image_file="gato.jpg",
+                           nn_cutoff=11)
+    run_style_transfer_pt1(out_dir="69",
+                           image_file="gato.jpg",
+                           nn_cutoff=13)
+    run_style_transfer_pt1(out_dir="69",
+                           image_file="gato.jpg",
+                           nn_cutoff=5)
 
 
 
