9f8ff021e56999ec8c688f50b988dc9d9f3be53b
Anderson dos Santos Paschoalon <anderson.paschoalon@gmail.com>
2023-12-11 00:23:52 -0300

    avanço no curso de gans

---
+++ Udemy/DeepLearning-GANsAutoencoders/Docs/install_theano.bat
@@ -0,0 +1,6 @@
+conda install -c conda-forge theano
+conda install -c -y "conda-forge/label/broken" theano
+conda install -c -y "conda-forge/label/cf201901" theano
+conda install -c -y "conda-forge/label/cf202003" theano
+conda install -c -y "conda-forge/label/gcc7" theano
+
--- Udemy/DeepLearning-GANsAutoencoders/Sources/util.py
+++ Udemy/DeepLearning-GANsAutoencoders/Sources/util.py
@@ -170,3 +170,45 @@ def download_file(file_id, dest):
         response = session.get(drive_url, params=params, stream=True)
 
     save_response_content(response, dest)
+
+
+def get_normalized_data():
+    print("Reading in and transforming data...")
+
+    if not os.path.exists('/large_files/digit-recognizer/train.csv'):
+        print('Looking for /large_files/digit-recognizer/train.csv')
+        print('You have not downloaded the data and/or not placed the files in the correct location.')
+        print('Please get the data from: https://www.kaggle.com/c/digit-recognizer')
+        print('Place train.csv in the folder large_files adjacent to the class folder')
+        exit()
+
+    df = pd.read_csv('/large_files/digit-recognizer/train.csv')
+    data = df.values.astype(np.float32)
+    np.random.shuffle(data)
+    X = data[:, 1:]
+    Y = data[:, 0]
+
+    Xtrain = X[:-1000]
+    Ytrain = Y[:-1000]
+    Xtest = X[-1000:]
+    Ytest = Y[-1000:]
+
+    # normalize the data
+    mu = Xtrain.mean(axis=0)
+    std = Xtrain.std(axis=0)
+    np.place(std, std == 0, 1)
+    Xtrain = (Xtrain - mu) / std
+    Xtest = (Xtest - mu) / std
+
+    return Xtrain, Xtest, Ytrain, Ytest
+
+
+def y2indicator(y):
+    N = len(y)
+    y = y.astype(np.int32)
+    ind = np.zeros((N, 10))
+    for i in range(N):
+        ind[i, y[i]] = 1
+    return ind
+
+
