e90d9a2e60af862b404fed0961c231f25087b11a
AndersonPaschoalon <anderson.paschoalon@gmail.com>
2024-11-16 22:06:34 -0300

    avançando no curso de opengl

--- Udemy/Computer-Graphics-with-OpenGL-and-Cpp/OpenGLCourseApp/01_ShadersFirstTriangle/OpenGLCourseApp/OpenGLCourseApp/main.cpp
+++ Udemy/Computer-Graphics-with-OpenGL-and-Cpp/OpenGLCourseApp/01_ShadersFirstTriangle/OpenGLCourseApp/OpenGLCourseApp/main.cpp
@@ -2,10 +2,8 @@
 //
 #include <stdio.h>
 #include <string.h>
-#include <cmath>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <glm/mat4x4.hpp>
 
 
 const GLint WIDTH = 800;
@@ -14,12 +12,6 @@ const GLint HEIGHT = 600;
 GLuint VAO;
 GLuint VBO;
 GLuint shader;
-GLuint uniformXMove;
-bool direction = true;
-float triOffset = 0.0f;
-float triMaxOffset = 0.7f;
-float triIncrement = 0.005f;
-
 
 // vertex shader
 static const char* vShader = "\n\
@@ -27,11 +19,9 @@ static const char* vShader = "\n\
 \n\
 layout (location = 0) in vec3 pos;\n\
 \n\
-uniform float xMove;\n\
-\n\
 void main()\n\
 {\n\
-	gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y + xMove, pos.z, 1.0);\n\
+	gl_Position = vec4(0.4 * pos.x, 0.6 * pos.y, pos.z, 5.0);\n\
 }";
 
 // fragment shader
@@ -109,8 +99,6 @@ void CompileShaders() {
 		printf("Error validating program: '%s'\n", eLog);
 		return;
 	}
-
-	uniformXMove = glGetUniformLocation(shader, "xMove");
 
 }
 
@@ -196,28 +184,16 @@ int main()
 
 
 	// Loop until window closed
+
 	while (!glfwWindowShouldClose(mainWindow)) {
 		// Get and handle user input events
 		glfwPollEvents();
-
-		if (direction) {
-			triOffset += triIncrement;
-		}
-		else {
-			triOffset -= triIncrement;
-		}
-		if (abs(triOffset) >= triMaxOffset) {
-			direction = !direction;
-		}
 
 		// clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shader);
-
-		glUniform1f(uniformXMove, triOffset);
-
 
 		glBindVertexArray(VAO);
 		// 3 - points we want to  draw
